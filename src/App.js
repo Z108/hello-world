43c9ec9e9d0b2cf788fc864f547329a935026f61
zgsgx <1084559628@qq.com>
2020-05-19 11:49:19 +0800

    state补充

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header'
-import Footer from './Footer'
+// import Footer from './Footer'
 class App extends React.Component{
   constructor(...args){
     super(...args)
@@ -11,9 +11,37 @@ class App extends React.Component{
     }
   }
   fn=()=>{
-    this.setState({
-      num:this.state.num+1
-    })
+    // this.setState({
+    //   num:this.state.num+1
+    // })
+    // this.setState({
+    //   num:this.state.num+1
+    // })                         //异步代码
+
+    // this.setState(function(oldstate){
+    //   return {
+    //     num:oldstate.num++
+    //   }
+    // })
+    // this.setState(function(oldstate){
+    //   return {
+    //     num:oldstate.num++
+    //   } 
+    // })                        //同步代码
+    // console.log(this.state.num)
+   
+    setTimeout(() => {
+      this.setState({
+          num:this.state.num++
+        })                       //异步代码
+
+        // this.setState(function(oldstate){
+        //   return {
+        //     num:oldstate.num++
+        //   } 
+        // })            //同步代码
+    }, 0);
+   
   }
   btn=(val)=>{
     console.log(val)
@@ -21,15 +49,14 @@ class App extends React.Component{
       str:val
     })
   }
-
   render(){
     let {num,str}=this.state
     return (
       <div>
         <Header tit='导航' num={num}/>
         <Header tit='头部' num={num}/>
-        <Header />
-        <Footer btn={this.btn} str={str}/>
+        {/* <Header /> */}
+        {/* <Footer btn={this.btn} str={str}/> */}
         <button onClick={this.fn}>add</button>
       </div>
     )
