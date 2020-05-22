7bb36cbf6edf968ed7f31d89aa3df6e2c3c6f2d0
zgsgx <1084559628@qq.com>
2020-05-22 17:48:37 +0800

    路由+fetch

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
 
 import './App.css';
 import React, { Component } from 'react'
-import Child from './component/Child'
+import qs from 'qs'
+import {Route} from 'react-router-dom'
+import Home from './component/Home'
 
-export default class App extends Component {
-  state={
-    name:'XXX'
-  }
-  fn=()=>{
-    console.log(111);
-  }
+export default class App extends React.Component {
+ componentDidMount(){
+   let data={
+     username:'',
+     password:''
+   }
+  fetch(
+    'http://vueshop.glbuys.com/api/home/user/pwdlogin?token=1ec949a15fb709370f',
+    {
+      method:'post', //请求方式
+      mode:'cors',  //跨域
+      headers:{     //设置请求头
+        "Content-Type":"application/x-www-form-urlencolded"
+      },
+      body:qs.stringify(data) //请求数据
+    }
+  ).then(res=>{
+    if(res.ok)
+    return res.json() //返回一个body对象，数据依然需要数据转换，转换过程中也是promise
+    else
+    return new Promise(res=>{res('请求资源找不到')})
+  }).then(res=>{
+    console.log(res);
+  })
+
+ }
   render() {
     return (
       <div>
-        aaa
-        {/* <Child>
-          <p>A1</p>
-          <p>A2</p>
-          <p>{this.state.name}</p>
-          <button onClick={this.fn}>点击</button>
-        </Child> */}
-
-        <Child render={(x,y)=>{
-          return <img width={250} height={250}  src="/logo192.png" style={{position:'fixed',left:x,top:y}}/>
-        }} />
+          <Route path='/home' component={Home}/>
       </div>
     )
   }
