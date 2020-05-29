41b298839412ebbb936311ec6508758304fca5a3
zgsgx <1084559628@qq.com>
2020-05-29 18:12:55 +0800

    修改1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+
+import { connect } from 'react-redux'
+
+import Home from './views/Home'
+import Login from './views/Login'
+
+class App extends Component {
+  render() {
+    
+    return (
+      <div>
+        <Switch>
+          <Route path="/login" component={Login} />
+
+          <Route path="/" render={() => {
+            if (this.props.isLogin) {
+              return (
+                <Switch>
+                  <Route path="/home" component={Home} />
+                  <Redirect to="/home" />
+                </Switch>
+              )
+            } else {
+              return <Redirect to="/login" />
+            }
+
+          }} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default connect(
+  (state) => {
+    return {
+      isLogin: state.isLogin
+    }
+  },
+  null
+)(App);
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-
+import http from '../../utils/http'
 import axios from 'axios';
-
+import {listSuccess,listDel,carAdd } from '../../action/index'
 class index extends Component {
     componentDidMount() {
         const { listData } = this.props;
         if (listData.length <= 0) {
-            axios.get('/data.json').then(res => {
+            http.get('./data.json').then(res => {
                 console.log(res.datas);
-                // this.props.listSuccess( res.data );
+                this.props.listSuccess( res.datas );
             });
         }
     }
@@ -28,7 +28,7 @@ class index extends Component {
         const { listData } = this.props;
         const { search } = this.state;
         res = listData.filter(v => {
-            return v.title.includes(search);
+            return v.tit.includes(search);
         })
         return res;
     }
@@ -48,7 +48,7 @@ class index extends Component {
                     filterListtData.map(v => {
                         return (
                             <div key={v.id}>
-                                {v.title} - {v.price}
+                                {v.tit} - {v.price}
                                 <button onClick={ ()=>listDel(v.id) }>删除</button>
                                 <button onClick={ ()=>carAdd(v) }>加入购物车</button>
                             </div>
@@ -65,14 +65,8 @@ export default connect(
         return { listData: state.listData }
     },
     {
-        listSuccess(pyload) {
-            return {type: 'list_success', pyload}
-        },
-        listDel(pyload) {
-            return {type: 'list_del', pyload}
-        },
-        carAdd(pyload) {
-            return {type: 'car_add', pyload}
-        }
+        listSuccess,
+        listDel,
+        carAdd
     }
 )(index);
